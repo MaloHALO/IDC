ade549d1f93be8a49b5864dd136244baf3f24cfa
Sergey Zlydnev <szlydnev2@gmail.com>
2025-04-03 16:22:09 +0300

    Remake

--- workspace/src/main.jsx
+++ workspace/src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import * as ReactDOMClient from 'react-dom/client';
-import { App } from './App.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const rootElement = document.getElementById('root');
-const root = ReactDOMClient.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </StrictMode>,
+)
